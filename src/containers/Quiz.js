8cdc57b1d13d4d0998fe486b9477d4800623f190
Ulugbek <u.dushanov@gmail.com>
2023-02-12 21:13:47 +0500

    feat: add main layout

---
+++ src/containers/Quiz.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Quiz.css';
+
+class Quiz extends React.Component {
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                <h1>Quiz</h1>
+            </div>
+        );
+    }
+}
+
+export default Quiz;
