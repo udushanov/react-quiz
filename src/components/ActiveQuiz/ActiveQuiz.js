13d0996cb81f6b07ab5c67a5ab6fa40e0264e4ec
Ulugbek <u.dushanov@gmail.com>
2023-02-13 23:14:23 +0500

    feat: add all necesseray components

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './ActiveQuiz.module.css';
+import AnswersList from "./AnswersList/AnswersList";
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>2.</strong>&nbsp;
+                How are you?
+            </span>
+            <small>4 from 12</small>
+        </p>
+        <AnswersList answers={props.answers}/>
+    </div>
+)
+
+export default ActiveQuiz;
--- src/containers/Quiz.js
+++ src/containers/Quiz.js
@@ -1,11 +1,30 @@
 import React from "react";
-import classes from './Quiz.css';
+import classes from './Quiz.module.css';
+import ActiveQuiz from "../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends React.Component {
+    state = {
+        quiz: [
+            {
+                answers: [
+                    {text: 'Question 1'},
+                    {text: 'Question 2'},
+                    {text: 'Question 3'},
+                    {text: 'Question 4'},
+                ]
+            }
+        ]
+    }
+
     render() {
         return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+
+
+                <div className={classes.QuizWrapper}>
+                    <h1>Give your answers</h1>
+                    <ActiveQuiz answers={this.state.quiz[0].answers}/>
+                </div>
             </div>
         );
     }
