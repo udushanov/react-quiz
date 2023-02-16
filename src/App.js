b54dd04e89c9fd342ef81b4cf5d0fe59c0877794
Ulugbek <u.dushanov@gmail.com>
2023-02-16 22:47:27 +0500

    feat: add notfoundpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./containers/Quiz/Quiz";
 import About from "./containers/About/About";
+import {Routes, Route, Link} from "react-router-dom";
+import NotFoundPage from "./containers/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
@@ -9,17 +11,24 @@ function App() {
             <nav>
                 <ul>
                     <li>
-                        <a href="/">Quiz</a>
+                        <Link to="/">Quiz</Link>
                     </li>
                     <li>
-                        <a href="/about">About</a>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/router">Rendering from Route</Link>
                     </li>
                 </ul>
             </nav>
         </div>
 
-        <Quiz />
-        <About />
+        <Routes>
+            <Route path={'/'} element={<Quiz />} />
+            <Route path={'/about'} element={<About />} />
+            <Route path={'*'} element={<NotFoundPage />} />
+        </Routes>
+
 
     </Layout>
   );
